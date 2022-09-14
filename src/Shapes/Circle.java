eba358df23921e0187f96c1ae13241ba8147c4aa
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-09-14 12:43:08 +0300

    Circle classes is described, test class is created

--- src/Shapes/Circle.java
+++ src/Shapes/Circle.java
@@ -1,4 +1,43 @@
 package Shapes;
 
-public class Circle {
+public class Circle extends Shape {
+  // Variables
+  protected double radius;
+
+  // Constructor (overloaded)
+  public Circle() {
+    filled = false;
+    color = "blue";
+    radius = 1;
+  }
+  public Circle(double r) {
+    filled = false;
+    color = "blue";
+    radius = r;
+  }
+  public Circle(double r, String c, boolean f) {
+    filled = f;
+    color = c;
+    radius = r;
+  }
+
+  // Methods
+  public double getRadius() {
+    return this.radius;
+  }
+  public void setRadius(double r) {
+    this.radius = r;
+  }
+  @Override
+  public double getArea() {
+    return Math.PI*radius*radius;
+  }
+  @Override
+  public double getPerimeter() {
+    return 2*Math.PI*radius;
+  }
+  @Override
+  public String toString() {
+    return "Area: " + this.getArea() + ", Perimeter: " + this.getPerimeter() + ", Color: " + this.color + ", Filled" + this.filled;
+  }
 }
---
+++ src/Shapes/Test.java
@@ -0,0 +1,7 @@
+package Shapes;
+
+public class Test {
+  public static void main(String[] args) {
+    Circle c1 = new Circle();
+  }
+}
