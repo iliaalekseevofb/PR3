97907081bc64193235c7381ea9e597e77db6972d
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-09-14 11:53:32 +0300

    Abstract parent class Shape is defined

--- src/Shapes/Shape.java
+++ src/Shapes/Shape.java
@@ -2,6 +2,31 @@ package Shapes;
 
 public abstract class Shape {
   // Variables
-  // Constructor
+  protected String color;
+  protected boolean filled;
+
+  // Constructor (overloaded)
+  public Shape() {}
+  public Shape(String c, boolean f) {
+    color = c;
+    filled = f;
+  }
+
   // Methods
+  public String getColor() {
+    return this.color;
+  }
+  public boolean isFilled() {
+    return this.filled;
+  }
+  public void setColor(String c) {
+    this.color = c;
+  }
+  public void setFilled(boolean f) {
+    this.filled = f;
+  }
+
+  public abstract double getArea();
+  public abstract double getPerimeter();
+  public abstract String toString();
 }
